40ae9c58428fdcbc39656500bc4b7f6af6ef55f9
vincentepos <Vincent.Grobler@eposability.com>
2018-09-26 14:19:23 +0200

    update
    update

--- OM/OM/Models/Constants.cs
+++ OM/OM/Models/Constants.cs
@@ -15,25 +15,46 @@ namespace OM.Models
 
         public static int LoginIconHeight = 120;
 
+        ////-------------Login------------//
+        //public static string LoginUrl = "https://app.opsimize.com/rest/mobileapp/v1/Token";
+
+        ////-------------PO List---------------//
+        //public static string PoUrl = "https://app.opsimize.com/rest/mobileapp/v1/PurchaceOrderList";
+
+        ////-------------PO Ref---------------//
+        //public static string PoRefUrl = "https://app.opsimize.com/rest/mobileapp/v1/PurchaceOrder"; // ?PORef=14114282
+
+        ////-------------PO Line List---------------//
+        //public static string PoLineUrl = "https://app.opsimize.com/rest/mobileapp/v1/PurchaceOrderLines"; // ?PORef=1411428
+
+        ////-------------Take Delivery---------------//
+        //public static string TakeDeliveryUrl = "https://app.opsimize.com/rest/mobileapp/v1/TakeDelivery"; // ?OrderRef=1411428
+
+        ////-------------DN Line List---------------//
+        //public static string DNLineUrl = "https://app.opsimize.com/rest/mobileapp/v1/TakeDelivery/GetLines"; // ?PORef=1411428
+
+        ////-------------Post Delivery---------------//
+        //public static string PostDeliveryUrl = "https://app.opsimize.com/rest/mobileapp/v1/TakeDelivery/Delivery"; 
+
         //-------------Login------------//
-        public static string LoginUrl = "http://opsimize.eposability.com/rest/mobileapp/v1/Token";
+        public static string LoginUrl = "http://10.0.2.2:8081/rest/mobileapp/v1/Token";
 
         //-------------PO List---------------//
-        public static string PoUrl = "http://opsimize.eposability.com/rest/mobileapp/v1/PurchaceOrderList";
+        public static string PoUrl = "http://10.0.2.2:8081/rest/mobileapp/v1/PurchaceOrderList";
 
         //-------------PO Ref---------------//
-        public static string PoRefUrl = "http://opsimize.eposability.com/rest/mobileapp/v1/PurchaceOrder"; // ?PORef=14114282
+        public static string PoRefUrl = "http://10.0.2.2:8081/rest/mobileapp/v1/PurchaceOrder"; // ?PORef=14114282
 
         //-------------PO Line List---------------//
-        public static string PoLineUrl = "http://opsimize.eposability.com/rest/mobileapp/v1/PurchaceOrderLines"; // ?PORef=1411428
+        public static string PoLineUrl = "http://10.0.2.2:8081/rest/mobileapp/v1/PurchaceOrderLines"; // ?PORef=1411428
 
         //-------------Take Delivery---------------//
-        public static string TakeDeliveryUrl = "http://opsimize.eposability.com/rest/mobileapp/v1/TakeDelivery"; // ?OrderRef=1411428
+        public static string TakeDeliveryUrl = "http://10.0.2.2:8081/rest/mobileapp/v1/TakeDelivery"; // ?OrderRef=1411428
 
         //-------------DN Line List---------------//
-        public static string DNLineUrl = "http://opsimize.eposability.com/rest/mobileapp/v1/TakeDelivery/GetLines"; // ?PORef=1411428
+        public static string DNLineUrl = "http://10.0.2.2:8081/rest/mobileapp/v1/TakeDelivery/GetLines"; // ?PORef=1411428
 
         //-------------Post Delivery---------------//
-        public static string PostDeliveryUrl = "http://opsimize.eposability.com/rest/mobileapp/v1/TakeDelivery/Delivery"; 
+        public static string PostDeliveryUrl = "http://10.0.2.2:8081/rest/mobileapp/v1/TakeDelivery/Delivery";
     }
 }
