22faf6f27e87c3f0d4c5f3235e2fdec958d8a76c
vincentepos <Vincent.Grobler@eposability.com>
2018-12-21 13:54:44 +0200

    Update IOS
    Update IOS

--- OM/OM.iOS/AppDelegate.cs
+++ OM/OM.iOS/AppDelegate.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using Firebase.CloudMessaging;
 using Foundation;
 using UIKit;
 using UserNotifications;
-using Firebase.CloudMessaging;
-using Firebase.Analytics;
+using Firebase.Core;
 
 namespace OM.iOS
 {
@@ -14,7 +14,7 @@ namespace OM.iOS
     // User Interface of the application, as well as listening (and optionally responding) to 
     // application events from iOS.
     [Register("AppDelegate")]
-    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate, IUNUserNotificationCenterDelegate
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate, IUNUserNotificationCenterDelegate, IMessagingDelegate
     {
         //
         // This method is invoked when the application has loaded and is ready to run. In this 
@@ -30,114 +30,41 @@ namespace OM.iOS
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
-            // get permission for notification
+            Firebase.Core.App.Configure();
+
+            Messaging.SharedInstance.Delegate = this;
+
+            // Register your app for remote notifications.
             if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
             {
-                // iOS 10
-                var authOptions = UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound;
-                UNUserNotificationCenter.Current.RequestAuthorization(authOptions, (granted, error) =>
-                {
-                    Console.WriteLine(granted);
-                });
 
                 // For iOS 10 display notification (sent via APNS)
                 UNUserNotificationCenter.Current.Delegate = this;
+
+                var authOptions = UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound;
+                UNUserNotificationCenter.Current.RequestAuthorization(authOptions, (granted, error) => {
+                    Console.WriteLine(granted);
+                });
             }
             else
             {
-
+                // iOS 9 or before
                 var allNotificationTypes = UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound;
                 var settings = UIUserNotificationSettings.GetSettingsForTypes(allNotificationTypes, null);
-
-
                 UIApplication.SharedApplication.RegisterUserNotificationSettings(settings);
             }
 
             UIApplication.SharedApplication.RegisterForRemoteNotifications();
 
-            // Firebase component initialize
-            Firebase.Core.App.Configure();
-
-            Firebase.InstanceID.InstanceId.Notifications.ObserveTokenRefresh((sender, e) =>
-            {
-                var newToken = Firebase.InstanceID.InstanceId.SharedInstance.Token;
-                // if you want to send notification per user, use this token
-                System.Diagnostics.Debug.WriteLine(newToken);
-
-                connectFCM();
-            });
-
             return base.FinishedLaunching(app, options);
-
         }
 
-        public override void DidEnterBackground(UIApplication uiApplication)
+        public void DidReceiveRegistrationToken(Messaging messaging, string fcmToken)
         {
-            Messaging.SharedInstance.Disconnect();
-        }
+            Console.WriteLine($"Firebase registration token: {fcmToken}");
 
-        public override void OnActivated(UIApplication uiApplication)
-        {
-            connectFCM();
-            base.OnActivated(uiApplication);
-        }
-
-        public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
-        {
-            Messaging.SharedInstance.ApnsToken = deviceToken;
-            Console.WriteLine("Firebase registration token: " + deviceToken);
-        }
-
-        // iOS 9 <=, fire when recieve notification foreground
-        public override void DidReceiveRemoteNotification(UIApplication application, NSDictionary userInfo, Action<UIBackgroundFetchResult> completionHandler)
-        {
-            Messaging.SharedInstance.AppDidReceiveMessage(userInfo);
-
-            // Generate custom event
-            NSString[] keys = { new NSString("Event_type") };
-            NSObject[] values = { new NSString("Recieve_Notification") };
-            var parameters = NSDictionary<NSString, NSObject>.FromObjectsAndKeys(keys, values, keys.Length);
-
-            // Send custom event
-            Firebase.Analytics.Analytics.LogEvent("CustomEvent", parameters);
-
-            if (application.ApplicationState == UIApplicationState.Active)
-            {
-                System.Diagnostics.Debug.WriteLine(userInfo);
-                var aps_d = userInfo["aps"] as NSDictionary;
-                var alert_d = aps_d["alert"] as NSDictionary;
-                var body = alert_d["body"] as NSString;
-                var title = alert_d["title"] as NSString;
-                debugAlert(title, body);
-            }
-        }
-
-        // iOS 10, fire when recieve notification foreground
-        [Export("userNotificationCenter:willPresentNotification:withCompletionHandler:")]
-        public void WillPresentNotification(UNUserNotificationCenter center, UNNotification notification, Action<UNNotificationPresentationOptions> completionHandler)
-        {
-            var title = notification.Request.Content.Title;
-            var body = notification.Request.Content.Body;
-            debugAlert(title, body);
-        }
-
-        private void connectFCM()
-        {
-            Messaging.SharedInstance.Connect((error) =>
-            {
-                if (error == null)
-                {
-                    //TODO: Change Topic to what is required
-                    Messaging.SharedInstance.Subscribe("/topics/all");
-                }
-                System.Diagnostics.Debug.WriteLine(error != null ? "error occured" : "connect success");
-            });
-        }
-
-        private void debugAlert(string title, string message)
-        {
-            var alert = new UIAlertView(title ?? "Title", message ?? "Message", null, "Cancel", "OK");
-            alert.Show();
+            // TODO: If necessary send token to application server.
+            // Note: This callback is fired at each app startup and whenever a new token is generated.
         }
     }
 }
